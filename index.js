89ee29758343038c8c351e1d055404357125c536
Umran <mohammedumran.0416@gmail.com>
2025-06-30 19:47:04 +0100

    I think all the styling is done, now time to work on the logic (maybe add a timer at the end)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+let playerScore = 0;
+let cpuScore = 0;
+let cpuChoice = ['rock','paper','scissors']
+let output = ['You win', 'CPU wins', "It's a tie"]
 
-
-let playerScore = document.getElementById("player-score").innerText = "Player : "
-let cpuScore = document.getElementById("CPU-score").innerText = "CPU : "
+document.getElementById("player-score").innerText = "Player : " + playerScore;
+document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
+document.getElementById("output").innerText = output[0]
+document.getElementById("computer-choice").innerText = "CPU choses " + cpuChoice[0]
 
