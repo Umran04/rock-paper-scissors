348d58e99b78daddd6b466f7cefe8c2bc8710c3d
Umran <mohammedumran.0416@gmail.com>
2025-07-01 10:50:42 +0100

    added the logic for the game, forgot to update in increments, but most of game is functional, code is very verbose

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ let cpuScore = 0;
 let cpuChoices = ['Rock','Paper','Scissors']
 let output = ['You win', 'CPU wins', "It's a tie"]
 let playerSelect = null; 
+let cpuSelect = null;
 
 let btnRock = document.getElementById("human-choice-rock");
 let btnPaper = document.getElementById("human-choice-paper");
 let btnScissors = document.getElementById("human-choice-scissors");
+let btnPlayAgain = document.getElementById("PA-btn"); 
 
 document.getElementById("player-score").innerText = "Player : " + playerScore;
 document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
@@ -15,22 +17,29 @@ document.getElementById("output").innerText = "No winner yet"
 
 
 
+
+function play(){
+    cpuSelect = getCPUchoice();
+    document.getElementById("computer-choice").innerText = "CPU choses " + cpuSelect;
+    getWinner();
+}
+
 btnRock.addEventListener('click', () => {
     playerSelect = 'Rock'
-    document.getElementById("computer-choice").innerText = "CPU choses " + getCPUchoice();
-    
+    play();
+    console.log('R')
 })
 
 btnPaper.addEventListener('click', () => {
     playerSelect = 'Paper'
-    document.getElementById("computer-choice").innerText = "CPU choses " + getCPUchoice();
-    
+    play();
+    console.log('P')
 })
 
 btnScissors.addEventListener('click', () => {
     playerSelect = 'Scissors'
-    document.getElementById("computer-choice").innerText = "CPU choses " + getCPUchoice();
-    
+    play();
+    console.log('S')
 })
 
 
@@ -39,3 +48,77 @@ function getCPUchoice(){
     return cpuChoices[randIndex]
 }
 
+function getWinner(){
+    if (playerSelect === cpuSelect){
+        document.getElementById("output").innerText = "It's a tie"
+    }else if (playerSelect === 'Rock' && cpuSelect === 'Paper'){
+        document.getElementById("output").innerText = "CPU wins"
+        cpuScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
+    }else if (playerSelect === 'Paper' && cpuSelect === 'Rock'){
+        document.getElementById("output").innerText = "You win, you chose " + playerSelect
+        playerScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("player-score").innerText = "Player : " + playerScore;
+    }else if (playerSelect === 'Paper' && cpuSelect === 'Scissors'){
+        document.getElementById("output").innerText = "CPU wins"
+        cpuScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
+    }else if (playerSelect === 'Scissors' && cpuSelect === 'Paper'){
+        document.getElementById("output").innerText = "You win, you chose " + playerSelect
+        playerScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("player-score").innerText = "Player : " + playerScore;
+    }else if (playerSelect === 'Scissors' && cpuSelect === 'Rock'){
+        document.getElementById("output").innerText = "CPU wins"
+        cpuScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
+    }else if (playerSelect === 'Rock' && cpuSelect === 'Scissors'){
+        document.getElementById("output").innerText = "You win, you chose " + playerSelect
+        playerScore++;
+        if (playerScore === 5 || cpuScore === 5){
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+            btnRock.disabled = true;
+        }
+        document.getElementById("player-score").innerText = "Player : " + playerScore;
+    }else{}
+}
+
+
+
+btnPlayAgain.addEventListener('click', () => {
+    playerScore = 0;
+    cpuScore = 0;
+    document.getElementById("CPU-score").innerText = "CPU : " + cpuScore;
+    document.getElementById("player-score").innerText = "Player : " + playerScore;
+    document.getElementById("computer-choice").innerText = "CPU wating for you to select"
+    document.getElementById("output").innerText = "No winner yet"
+
+    btnPaper.disabled = false;
+    btnScissors.disabled = false;
+    btnRock.disabled = false;
+})
